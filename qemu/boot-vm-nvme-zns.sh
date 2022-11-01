7734492c99b302072b7fa2fff8da6fe32034de32
Animesh Trivedi <animesh.trivedi@gmail.com>
2022-11-01 11:33:04 +0100

    a working qemu command

--- qemu/boot-vm-nvme-zns.sh
+++ qemu/boot-vm-nvme-zns.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #ubuntu-2004.qcow2 or ubuntu-20.04-stosys-v5.12.qcow
-QEMU_PATH="/home/atr/src/qemu-6.1.0/build/"
+QEMU_HOME="/home/atr/src/qemu-6.1.0/build/"
+QEMU_BUILD=$QEMU_HOME/build/
 OS_IMG_PATH="/mnt/sdb/atr/ubuntu-20.04-stosys-v5.12.qcow"
 NVME1_PATH="/mnt/sdb/atr/nvmessd-4G.img"
 #ZNS_PATH="/mnt/sdb/4tb/atr/"
 #-drive file=znsssd-32M.img,id=zns-device,format=raw,if=none \
 
 set -ex
-sudo $QEMU_PATH/qemu-system-x86_64 \
--name qemuzns -m 32G --enable-kvm -cpu host -smp 4 -accel kvm \
+sudo $QEMU_BUILD/qemu-system-x86_64 \
+-name qemunvme-$((1 + $RANDOM % 10)) -m 32G --enable-kvm -cpu host -smp 4 \
 -hda $OS_IMG_PATH \
--drive file=$NVME1_PATH,id=nvme-device,format=raw,if=none \
--device nvme,drive=nvme-device,serial=nvme-dev,physical_block_size=4096,logical_block_size=4096
 -net user,hostfwd=tcp::7777-:22 -net nic \
--netdev bridge,id=hn0,br=virbr0 \
--device virtio-net-pci,netdev=hn0,mac=e6:c8:ff:09:76:99,id=nic1
+-drive file=$NVME1_PATH,id=nvme-device,format=raw,if=none \
+-device nvme,drive=nvme-device,serial=nvme-dev,physical_block_size=512,logical_block_size=512 \
+-netdev bridge,id=hn0,br=br1,helper=$QEMU_BUILD/qemu-bridge-helper -device virtio-net-pci,netdev=hn0,id=nic1 #\mac=e6:c8:ff:09:76:99,
