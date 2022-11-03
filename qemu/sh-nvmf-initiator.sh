7dd0b3e2b54962cf2b41d5549b09c3c3b987b3a6
Animesh Trivedi <animesh.trivedi@gmail.com>
2022-11-03 11:39:16 +0100

    nvmf scripts

---
+++ qemu/sh-nvmf-initiator.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+NVME_DEVICE="/dev/nvme0n1"
+SERVER_IP_ADDR="190.160.10.8"
+SERVER_IP_PORT=4420
+NAMESPACE_NAME="nvme-atr-target"
+
+set -e
+sudo modprobe nvme
+sudo modprobe nvme-tcp
+echo "1. modprobe done..."
+
+echo "2. discovering...."
+sudo nvme discover -t tcp -a $SERVER_IP_ADDR -s $SERVER_IP_PORT --hostnqn=nqn.2014-08.org.nvmexpress:uuid:1b4e28ba-2fa1-11d2-883f-0016d3ccabcd
+
+echo "3. connecting..."
+sudo nvme connect -t tcp -n $NAMESPACE_NAME -a $SERVER_IP_ADDR -s $SERVER_IP_PORT --hostnqn=nqn.2014-08.org.nvmexpress:uuid:1b4e28ba-2fa1-11d2-883f-0016d3ccabcd
+
+echo "4. show list"
+sudo nvme list
+
+# how to disconnect
+# sudo nvme disconnect /dev/nvme0n1 -n nvme-test-target
---
+++ qemu/sh-nvmf-target.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+NVME_DEVICE="/dev/nvme0n1"
+SERVER_IP_ADDR="190.160.10.8"
+SERVER_IP_PORT=4420
+NAMESPACE_NAME="nvme-atr-target"
+
+set -e
+sudo modprobe nvmet
+sudo modprobe nvmet-tcp
+echo "1. modprobe done..."
+
+cd /sys/kernel/config/nvmet/subsystems
+sudo mkdir $NAMESPACE_NAME
+cd $NAMESPACE_NAME/
+echo 1 | sudo tee -a attr_allow_any_host > /dev/null
+sudo mkdir namespaces/1
+cd namespaces/1
+echo -n $NVME_DEVICE |sudo tee -a device_path > /dev/null
+echo 1|sudo tee -a enable > /dev/null
+echo "2. a target namespace done..."
+
+sudo mkdir /sys/kernel/config/nvmet/ports/1
+cd /sys/kernel/config/nvmet/ports/1
+echo $SERVER_IP_ADDR | sudo tee -a addr_traddr > /dev/null
+echo tcp | sudo tee -a addr_trtype > /dev/null
+echo $SERVER_IP_PORT |sudo tee -a addr_trsvcid > /dev/null
+echo ipv4 | sudo tee -a addr_adrfam > /dev/null
+echo "3. port setup done..."
+
+sudo ln -s /sys/kernel/config/nvmet/subsystems/nvme-test-target/ /sys/kernel/config/nvmet/ports/1/subsystems/nvme-test-target
+echo "4. linking done...see the dmesg log output (last 10 lines)"
+dmesg | tail -10
