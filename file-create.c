c0a64f86a86d0e18aaa314b9e97f674002c172d0
Animesh Trivedi <animesh.trivedi@gmail.com>
2022-11-04 13:11:33 +0100

    updated fsync on dir

--- file-create.c
+++ file-create.c
@@ -4,16 +4,34 @@
 #include <errno.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/time.h>
 
 #define DATA_SIZE 1000
 /* Variable to store user content */
 char data[DATA_SIZE];
 char path[]="/mnt/nvmf-tcp/atr-bench/";
 
-int create_single(char *fpath) {
+static unsigned long long epoch_time(){
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    unsigned long long usec =
+            (unsigned long long) (tv.tv_sec) * 1000000 +
+            (unsigned long long) (tv.tv_usec);
+    return usec;
+}
+
+int create_single(char *fpath, int dirfd) {
     int fd = open(fpath, O_CREAT | O_WRONLY | O_APPEND | O_CREAT, 0644);
     if (fd < 0) {
-        printf("file opening failed errno %d \n", errno);
+        printf("file opening failed path %s errno %d \n", fpath, errno);
+        return -1;
+    }
+    if(fsync(fd) != 0){
+        printf("file fsync failed path %s errno %d \n", fpath, errno);
+        return -1;
+    }
+    if(fsync(dirfd) != 0){
+        printf("file fsync DIR failed path %s errno %d \n", fpath, errno);
         return -1;
     }
     close(fd);
@@ -32,12 +50,23 @@ int main(int argc, char **argv) {
     if(argc == 2){
         times = atoi(argv[1]);
     }
+    // open the dirfd
+    int dirfd = open(path, O_WRONLY, 0644);
+    if(dirfd < 0){
+        printf("opening of the directory path failed at %s errno %d \n", path, errno);
+        return -1;
+    }
+
+    long long int s = epoch_time();
     for(int i = 0; i < times; i++){
         snprintf(enumPath, sizeAlloc, "%s/%d", path, i);
-        if(create_single(enumPath) != 0){
+        if(create_single(enumPath, dirfd) != 0){
             printf("Error failed, break \n");
             break;
         }
     }
+    long long int e = epoch_time();
+    close(dirfd);
+    printf(" %d files at %s directory took %llu milliseconds\n", times, path, (e-s)/1000);
     return 0;
 }
